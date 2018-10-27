56fe60f073c6d22c3a77157e2148944a458a10b3
weemzz <weemzz@gmail.com>
2018-10-27 14:47:18 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -105,3 +105,20 @@ $(document).ready(function(){
  function unhover(element) {
    element.setAttribute('src', 'img/weem.png');
  }
+
+ // When the user scrolls down 20px from the top of the document, show the button
+ window.onscroll = function() {scrollFunction()};
+
+ function scrollFunction() {
+     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+         document.getElementById("wimBtn").style.display = "block";
+     } else {
+         document.getElementById("wimBtn").style.display = "none";
+     }
+ }
+
+ // When the user clicks on the button, scroll to the top of the document
+ function topFunction() {
+     document.body.scrollTop = 0; // For Safari
+     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+ }
